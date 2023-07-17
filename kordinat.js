201ac8eea3f14368651e0e5df5fd145aa560eef6
Nahid <115393748+Nahidtyev@users.noreply.github.com>
2023-07-17 23:02:51 +0300

    Add files via upload

---
+++ kordinat.js
@@ -0,0 +1,11 @@
+
+switch(x,y){
+    case x>0 && y>0:
+        console.log("I section");
+    case x<0 && y>0:
+        console.log("II section");
+    case x<0 && y<0:
+        console.log("III section");
+    case x>0 && y<0:
+        console.log("IV section");
+}
